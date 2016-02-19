12c8bd6f60669bd10b199862833c0a166cdfb9e1
River Valadão <rivervaladao@gmail.com>
2016-02-19 17:50:29 -0200

    Initial commit

---
+++ app/src/main/java/com/example/fabrica/adaptivescenery/ImageFragment.java
@@ -0,0 +1,20 @@
+package com.example.fabrica.adaptivescenery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by fabrica on 19/02/16.
+ */
+public class ImageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image, container, false);
+    }
+}
